5fa7e527515e182d46eebef744f0a9226dfbe04d
Mateus Paulart <mateusp23@rede.ulbra.br>
2022-09-24 19:59:24 -0300

    evoluindo o layout com nativeBase

--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Heading, Icon, useTheme, Text } from 'native-base';
+import { VStack, Heading, Icon, useTheme, Text, Center, Box } from 'native-base';
 
 import Logo from "../assets/logo_little.svg";
 import { Button } from '../components/Button';
@@ -7,15 +7,18 @@ export function SignIn() {
   const { colors } = useTheme();
 
   return (
-    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={40}>
+    <Center height="full" bg="gray.600">
       <Logo />
+      <Box width="full" p={5}>
 
-      <Text color="white" fontSize="xl" mt={16} mb={6}>
-        Acesse sua conta
-      </Text>
-
-      <Button bg="gray.650" color="primary.700" title="Entrar com Google" w="full" mb={6}/>
-      <Button bg="primary.700" color="white" title="Entrar com Apple" w="full" mb={6}/>
-    </VStack>
+        <Text color="white" textAlign="center" fontSize="xl" mt={6} mb={6}>
+          Acesse sua conta
+        </Text>
+        <VStack >
+          <Button bg="gray.650" color="primary.700" title="Entrar com Google" mb={6}/>
+          <Button bg="primary.700" color="white" title="Entrar com Apple" mb={6}/>
+        </VStack>
+      </Box>
+    </Center>
   );
 }

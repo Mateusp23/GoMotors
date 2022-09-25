1c9dfde24c4597bc8166a4c5a73ced00f617ab5f
Mateus Paulart <mateusp23@rede.ulbra.br>
2022-09-25 18:17:04 -0300

    add lib phosphor-icons e terminando layout da tela SignIn

--- src/components/ButtonSignIn.tsx
+++ src/components/ButtonSignIn.tsx
@@ -1,23 +1,39 @@
-import { Stack, Icon, Button as ButtonSignIn} from "native-base";
-
-import IconGoogle from "../assets/icon-google.svg";
+import { Stack, Box, HStack, Heading, Divider, Icon, Button as BtnSignIn, useTheme } from 'native-base';
+import { GoogleLogo, AppleLogo } from 'phosphor-react-native';
 
 export function ButtonSignIn() {
+  const { colors } = useTheme();
+
   return (
-    <Stack direction={{
-      base: "column",
-      md: "row"
-    }} space={4}>
-      <ButtonSignIn 
+    <Box width="full" p={5}>
+      <BtnSignIn 
+        bg="gray.650"
+        h={14}
+        fontSize="sm"
+        rounded="sm"
+        _pressed={{ bg: "gray.700" }}
+      >
+        <HStack height="full" alignItems="center" justifyContent="center">
+          <Icon as={<GoogleLogo color={colors.primary[700]} size={32} />} />
+          <Divider height="full" thickness="2" bg="primary.700" h="full" mx="4" orientation="vertical" />
+          <Heading color="primary.700" fontSize="xl">Entrar com Google</Heading>
+        </HStack>
+      </BtnSignIn>
+      
+      <BtnSignIn 
         bg="primary.700"
         h={14}
         fontSize="sm"
         rounded="sm"
-        _pressed={{ bg: "primary.400" }}
-        leftIcon={<IconGoogle />}
+        _pressed={{ bg: "gray.700" }}
+        mt={4}
       >
-        Entrar com Google
-      </ButtonSignIn>
-    </Stack>
+        <HStack height="full" alignItems="center" justifyContent="center">
+          <Icon as={<AppleLogo color={colors.white} size={32} />} />
+          <Divider height="full" thickness="2" bg="white" h="full" mx="4" orientation="vertical" />
+          <Heading color="white" fontSize="xl">Entrar com a Apple</Heading>
+        </HStack>
+      </BtnSignIn>
+    </Box>
   );
 }
--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -2,6 +2,7 @@ import { VStack, Heading, Icon, useTheme, Text, Center, Box } from 'native-base'
 
 import Logo from "../assets/logo_little.svg";
 import { Button } from '../components/Button';
+import { ButtonSignIn } from '../components/ButtonSignIn';
 
 export function SignIn() {
   const { colors } = useTheme();
@@ -9,14 +10,13 @@ export function SignIn() {
   return (
     <Center height="full" bg="gray.600">
       <Logo />
-      <Box width="full" p={5}>
+      <Box width="full" p={6}>
 
-        <Text color="white" textAlign="center" fontSize="xl" mt={6} mb={6}>
+        <Text color="white" textAlign="center" fontSize="xl" mt={3} mb={3}>
           Acesse sua conta
         </Text>
-        <VStack >
-          <Button bg="gray.650" color="primary.700" title="Entrar com Google" mb={6}/>
-          <Button bg="primary.700" color="white" title="Entrar com Apple" mb={6}/>
+        <VStack>
+          <ButtonSignIn />
         </VStack>
       </Box>
     </Center>

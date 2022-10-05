36544e48a509036fb2828435fce943a27c6082b5
Mateus Paulart <mateusp23@rede.ulbra.br>
2022-10-04 23:03:06 -0300

    add numerico no campo de telefone

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from "react";
 
 export interface AuthProps {
   name: string;
@@ -14,23 +14,23 @@ export const AuthContext = createContext({} as AuthProps);
 function AuthProvider({ children }) {
   const [user, setUser] = useState({});
 
-  const signIn = ({ name, email, id, given_name, picture }: AuthProps ) => {
-    setUser({ 
+  const signIn = ({ name, email, id, given_name, picture }: AuthProps) => {
+    setUser({
       email: email,
       name: name,
       id: id,
       given_name: given_name,
       picture: picture,
     });
-  }
+  };
 
   return (
     <AuthContext.Provider value={{ signIn, user }}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
 
 const useAuth = () => useContext(AuthContext);
 
-export { AuthProvider, useAuth }
+export { AuthProvider, useAuth };
--- src/screens/EditMotoboy.tsx
+++ src/screens/EditMotoboy.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { VStack, Heading, useTheme } from 'native-base';
-import { Header } from '../components/Header';
-import { Input } from '../components/Forms/Input';
-import { Button } from '../components/Button';
+import React from "react";
+import { VStack, Heading, useTheme } from "native-base";
+import { Header } from "../components/Header";
+import { Input } from "../components/Forms/Input";
+import { Button } from "../components/Button";
 
 export function EditMotoboy() {
   const { colors } = useTheme();
@@ -11,10 +11,15 @@ export function EditMotoboy() {
     <VStack flex={1} p={5} bg="gray.600">
       <Header isBackScreen title="Editar Perfil - Motoboy" />
 
-      <Input type="text" placeholder="Quanto tempo ja realiza entregas?" isRequired />
-      <Input type="tel" placeholder="Telefone" mt={3} isRequired/>
-      <Input type="text" placeholder="Chave pix" mt={3} isRequired/>
-      <Input 
+      <Input
+        type="text"
+        placeholder="Quanto tempo ja realiza entregas?"
+        isRequired
+      />
+      <Input keyboardType="numeric" placeholder="Telefone" mt={3} />
+
+      <Input type="text" placeholder="Chave pix" mt={3} isRequired />
+      <Input
         placeholder="Conte um pouco de sua experiência"
         type="text"
         flex={1}
